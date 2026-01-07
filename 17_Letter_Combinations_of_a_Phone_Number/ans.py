fed945ef068a6afd322c862fbbf3974ab4fc2e38
withbenny <bennychang.ee12@nycu.edu.tw>
2026-01-07 19:29:30 +0800

    update

---
+++ 17_Letter_Combinations_of_a_Phone_Number/ans.py
@@ -0,0 +1,8 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        
+
+if __name__ == "__main__":
+    sol = Solution()
+    digits = "23"
+    print(sol.letterCombinations(digits))
