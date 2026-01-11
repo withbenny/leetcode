7e92c25480d665f2294b5d9fd590339c3e0135bd
withbenny <bennychang.ee12@nycu.edu.tw>
2026-01-12 06:44:12 +0800

    update

---
+++ 392_Is_Subsequence/ans.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        ps = pt = 0
+
+        while ps < len(s) and pt < len(t):
+            if s[ps] == t[pt]:
+                ps += 1
+            pt += 1
+
+        if ps == len(s):
+            return True
+        else:
+            return False
+
+if __name__ == "__main__":
+    sol = Solution()
+    s = "acb"
+    t = "ahbgdc"
+    print(sol.isSubsequence(s, t))
---
+++ 443_String_Compression/ans.py
@@ -0,0 +1,29 @@
+from typing import List
+
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        write = 0
+        read = 0
+        n = len(chars)
+
+        while read < n:
+            c = chars[read]
+            count = 0
+            while read < n and chars[read] == c:
+                read += 1
+                count += 1
+            
+            chars[write] = c
+            write += 1
+
+            if count > 1:
+                for digit in str(count):
+                    chars[write] = digit
+                    write += 1
+        
+        return write
+
+if __name__ == "__main__":
+    sol = Solution()
+    chars = ["a","a","b","b","c","c","c"]
+    print(sol.compress(chars))
