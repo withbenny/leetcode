6f1bfca816495d8f1fe0b4c247921860b3f1b5f7
withbenny <bennychang.ee12@nycu.edu.tw>
2026-01-11 15:52:09 +0800

    update

---
+++ 1768_Merge_Strings_Alternately/ans.py
@@ -0,0 +1,17 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        result = []
+        
+        for a, b in zip(word1, word2):
+            result.append(a + b)
+        
+        result.append(word1[len(word2):])
+        result.append(word2[len(word1):])
+
+        return "".join(result)
+
+if __name__ == "__main__":
+    sol = Solution()
+    word1 = "ac"
+    word2 = "pqrd"
+    print(sol.mergeAlternately(word1, word2))
